f5a4c0a447e6bd6b45e4e23c617fd1656db6ca11
SimoHellgren <hellgrensimo@gmail.com>
2022-06-11 13:41:52 +0300

    extracted global variable

--- db.py
+++ db.py
@@ -1,21 +1,22 @@
 import os
 import json
 
+BASEPATH = "./recipes"
+
 
 def read_recipes():
-    basepath = "./recipes"
-    for filename in os.listdir(basepath):
+    for filename in os.listdir(BASEPATH):
         if not filename.endswith("json"):
             continue
 
-        with open(os.path.join(basepath, filename), "r", encoding="utf-8") as f:
+        with open(os.path.join(BASEPATH, filename), "r", encoding="utf-8") as f:
             yield json.load(f)
 
 
 def create_recipe(filename, name, ingredients, method, tags=None):
     if not tags:
         tags = []
-    with open(f"./recipes/{filename}", "w") as f:
+    with open(os.path.join(BASEPATH, filename), "w") as f:
         json.dump(
             {"name": name, "ingredients": ingredients, "method": method, "tags": tags},
             f,

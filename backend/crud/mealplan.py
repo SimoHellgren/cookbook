86e44cd987c8264f93095cea3372ec98f6258746
SimoHellgren <hellgrensimo@gmail.com>
2022-06-30 22:01:28 +0300

    add get and update mealplan

--- backend/crud/mealplan.py
+++ backend/crud/mealplan.py
@@ -7,12 +7,34 @@ def get_all():
         return cur_to_dicts(cur)
 
 
+def get(id):
+    with SQLite(DB) as cur:
+        cur.execute('SELECT * FROM mealplan WHERE id = ?', (id,))
+        return cur_to_dicts(cur)[0]
+
+
 def create(date, name, servings):
     with SQLite(DB) as cur:
         cur.execute('INSERT INTO mealplan(date, name, servings) VALUES (?,?,?)', (date, name, servings))
 
         cur.execute('SELECT * FROM mealplan WHERE date = ? AND name = ?', (date, name))
 
+        return cur_to_dicts(cur)[0]
+
+
+def update(id, date, name, servings, recipe_id):
+    with SQLite(DB) as cur:
+        cur.execute(
+            'UPDATE mealplan SET '
+            'date = ? '
+            'name = ? '
+            'servings = ? '
+            'recipe_id = ? '
+            'WHERE id = ?',
+            (date, name, servings, recipe_id, id)
+        )
+
+        cur.execute('SELECT * FROM mealplan WHERE id = ?', (id,))
         return cur_to_dicts(cur)[0]
 
 

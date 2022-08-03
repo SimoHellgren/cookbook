0719847a1cfddd7fd3dda941afca54cef2d7d063
SimoHellgren <hellgrensimo@gmail.com>
2022-08-03 22:47:56 +0300

    basic mealplan page redone

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,8 +3,7 @@ let state = {
   recipes: [],
   ingredients: [],
   recipe_ingredients: [],
-  mealplans: [],
-  search: null
+  mealplans: []
 }
 
 const APIURL = "http://localhost:8000"
@@ -46,24 +45,32 @@ let api = (function() {
 
 // DOM manipulation
 let D = document
+let $body = D.querySelector("body")
 let $app = D.getElementById("app")
 
 //components
 const Navbar = () => {
-  const pages = ["Recipes", "Add recipe", "Mealplan", "Shopping list"]
+  const pages = [
+    ["Recipes", render(RecipesPage)], 
+    ["Add recipe", () => alert("Not implemented :)")], 
+    ["Mealplan", render(MealplanPage)], 
+    ["Shopping list", () => alert("Not implemented :)")]
+  ]
   
   let nav = D.createElement("nav")
   let ul = D.createElement("ul")
 
-  pages.forEach(page => {
+  pages.forEach(([page, callback]) => {
     let li = D.createElement("li")
     li.innerHTML = `<span class="navbar-link">${page}</span>`
+    li.onclick = callback
     ul.appendChild(li)
   })
 
   nav.append(ul)
-
-  return nav  
+  
+  // tbh not a huge fan of this
+  $body.replaceChild(nav, $body.querySelector("nav"))
 }
 
 const TagGrid = (tags) => {
@@ -152,39 +159,62 @@ const RecipeSearch = () => {
     return $sidebar
 }
 
-//pages
-const RecipesPage = () => {
-   $app.replaceChildren(
-    Navbar(),
-    RecipeSearch(),
-    RecipeGrid(state.recipes)
-   )
+const MealCard = (mealplan) => {
+  let card = D.createElement("div")
+  card.className = "mealcard"
+  card.setAttribute("id", mealplan.id)
+  card.setAttribute("data-date", mealplan.date)
+
+  let header = D.createElement("div")
+  header.className = "mealcard-header"
+  header.textContent = `${mealplan.date} ${mealplan.name} (${mealplan.servings})`
+
+  let recipedropdown = D.createElement("select")
+  recipedropdown.append(
+    new Option("", null, true), //empty choice as default
+    ...state.recipes.map(r => new Option(r.name, r.id, false, false))
+  )
+
+  recipedropdown.value = mealplan.recipe_id
+
+  card.append(header, recipedropdown)
+
+  return card
 }
 
-api.recipes.get()
-  .then(data => state.recipes = data)
-  .then(RecipesPage)
+const Mealplan = (mealplans) => {
+  return mealplans.map(MealCard)
+}
+
+//pages
+const render = page => {
+  return () => {
+    Navbar()
+    $app.replaceChildren(...page())
+  }
+}
+
+const RecipesPage = () => {
+  return [
+    RecipeSearch(),
+    RecipeGrid(state.recipes)
+  ]
+}
+
+const MealplanPage = () => {
+  return [
+    ...Mealplan(state.mealplans)
+  ]
+}
+
 
 //Event handlers
 
-// const onSearchSubmit = (event) => {
-//   //consider hiding cards instead of destroying and recreating the elements
-//   event.preventDefault()
-//   drawRecipeGrid(state.recipes.filter(r => r.name.toLowerCase().includes($searchvalue.value.toLowerCase())))
-// }
 
-// $searchform.onsubmit = onSearchSubmit
+//Initial state
+api.recipes.get()
+  .then(data => state.recipes = data)
+  .then(render(RecipesPage))
 
-// //Initial state
-// api.recipes.get()
-//   .then(data => state.recipes = data)
-//   .then(() => drawRecipeGrid(state.recipes))
-//   .then(_ => {
-//     // set search tags
-//     let container = D.getElementById("search-tags")
-//     alltags = new Set(state.recipes.map(r => r.tags.split(",")).flat().filter(e => e !== ""))
-
-//   api.ingredients.get().then(data => state.ingredients = data)
-//   api.recipe_ingredients.get().then(data => state.recipe_ingredients = data)
-//   api.mealplans.get().then(data => state.mealplans = data)
-//   })
+api.mealplans.get()
+  .then(data => state.mealplans = data)

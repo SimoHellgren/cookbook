09da104ad5497c4236492dbb14b61d550186e7e5
SimoHellgren <hellgrensimo@gmail.com>
2022-08-03 21:47:03 +0300

    rework main page with js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,8 +1,13 @@
-const APIURL = "http://localhost:8000"
 
 let state = {
-  recipes: []
+  recipes: [],
+  ingredients: [],
+  recipe_ingredients: [],
+  mealplans: [],
+  search: null
 }
+
+const APIURL = "http://localhost:8000"
 
 //api
 let api = (function() {
@@ -11,33 +16,79 @@ let api = (function() {
   const endpoint = path => {
     return {
       get: () => fetch(APIURL + path).then(r => r.json()),
-      getId: id => fetch(APIURL + path + `/${id}`).then(r => r.json()),
+      getId: id => fetch(APIURL + path + "/" + id).then(r => r.json()),
       post: data => fetch(APIURL + path, {
         method: "POST",
         headers: {
           'Content-Type': 'application/json'
         },
         body: JSON.stringify(data)
-      }).then(r => r.json())
+      }).then(r => r.json()),
+      put: (id, data) => fetch(APIURL + path + "/" + id, {
+        method: "PUT",
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+      })
     }
   }
   
   //expose endpoints
   return {
       recipes: endpoint("/recipes"),
-      ingredients: endpoint("/ingredients")
+      ingredients: endpoint("/ingredients"),
+      recipe_ingredients: endpoint("/recipe_ingredients"),
+      mealplans: endpoint("/mealplans")
   }
   
 })()
 
-
 // DOM manipulation
 let D = document
-let $recipegrid = D.getElementById("recipe-grid")
-let $searchform = D.getElementById("search")
-let $searchvalue = D.getElementById("search-value")
+let $app = D.getElementById("app")
 
-const createRecipeGridCard = ({ id, name, servings, tags }) => {
+//components
+const Navbar = () => {
+  const pages = ["Recipes", "Add recipe", "Mealplan", "Shopping list"]
+  
+  let nav = D.createElement("nav")
+  let ul = D.createElement("ul")
+
+  pages.forEach(page => {
+    let li = D.createElement("li")
+    li.innerHTML = `<span class="navbar-link">${page}</span>`
+    ul.appendChild(li)
+  })
+
+  nav.append(ul)
+
+  return nav  
+}
+
+const TagGrid = (tags) => {
+  let taggrid = D.createElement("div")
+  taggrid.className = "tag-grid"
+
+  let elements = tags.map(tag => {
+    let elem = D.createElement("div")
+    elem.className = "tag"
+    elem.textContent = tag
+
+    elem.onclick = (ev) => {
+      ev.stopPropagation()
+      alert("You clicked on tag " + tag)
+    }
+
+    return elem
+  })
+
+  taggrid.append(...elements)
+
+  return taggrid
+}
+
+const RecipeCard = ({ id, name, servings, tags }) => {
   let card = D.createElement("div")
   card.setAttribute("class", "recipe-card")
   card.onclick = () => window.open(`recipe.html?recipe=${id}`)
@@ -46,54 +97,94 @@ const createRecipeGridCard = ({ id, name, servings, tags }) => {
   h2.textContent = `${name} (${servings})`
   card.appendChild(h2)
 
-  let taggrid = D.createElement("div")
-  taggrid.setAttribute("class", "tag-grid")
-
   if (tags !== "") {
-    tags.split(",").forEach(tag => {
-      let e = D.createElement("div")
-      e.setAttribute("class", "tag")
-      e.textContent = tag
-      e.onclick = (event) => {
-        event.stopPropagation()
-        alert(`You clicked on tag ${tag}`)
-      }
-      taggrid.appendChild(e)
-    })
-
+    let taggrid = TagGrid(tags.split(","))
     card.appendChild(taggrid)
   }
 
   return card;
 }
 
-const drawRecipeGrid = (recipes) => {
-  let cards = recipes.map(createRecipeGridCard)
-  $recipegrid.replaceChildren(...cards)
+const RecipeGrid = (recipes) => {
+  let $grid = D.createElement("div")
+  $grid.className = "recipe-grid"
+  let cards = recipes.map(RecipeCard)
+  $grid.replaceChildren(...cards)
+
+  return $grid
 }
 
-//Event handlers
-const onSearchSubmit = (event) => {
-  //consider hiding cards instead of destroying and recreating the elements
-  event.preventDefault()
-  drawRecipeGrid(state.recipes.filter(r => r.name.toLowerCase().includes($searchvalue.value.toLowerCase())))
+const RecipeSearch = () => {
+    let $sidebar = D.createElement("div")
+    $sidebar.className = "sidebar"
+
+    let p = D.createElement("p")
+    p.textContent = "Search recipes"
+
+    let form = document.createElement("form")
+    
+    let input = D.createElement("input")
+    input.placeholder = "Search by name"
+    
+    let submit = D.createElement("input")
+    submit.setAttribute("type", "submit")
+    submit.setAttribute("hidden", "")
+
+    form.append(input, submit)
+
+    form.onsubmit = (ev) => {
+      ev.preventDefault()
+
+      let data = state.recipes.filter(r => r.name.toLowerCase().includes(input.value.toLowerCase()))
+      let $grid = D.querySelector(".recipe-grid")
+
+      $grid.replaceWith(RecipeGrid(data))
+    }
+
+    let alltags = new Set(state.recipes.map(r => r.tags.split(",")).flat().filter(e => e !== ""))
+
+    $sidebar.append(
+      p,
+      form,
+      TagGrid([...alltags])
+    )
+
+    return $sidebar
 }
 
-$searchform.onsubmit = onSearchSubmit
+//pages
+const RecipesPage = () => {
+   $app.replaceChildren(
+    Navbar(),
+    RecipeSearch(),
+    RecipeGrid(state.recipes)
+   )
+}
 
-//Initial state
 api.recipes.get()
   .then(data => state.recipes = data)
-  .then(() => drawRecipeGrid(state.recipes))
-  .then(_ => {
-    // set search tags
-    let container = D.getElementById("search-tags")
-    alltags = new Set(state.recipes.map(r => r.tags.split(",")).flat().filter(e => e !== ""))
+  .then(RecipesPage)
 
-    alltags.forEach(t => {
-      let e = D.createElement("div")
-      e.setAttribute("class", "tag")
-      e.textContent = t
-      container.appendChild(e)
-    })
-  })
+//Event handlers
+
+// const onSearchSubmit = (event) => {
+//   //consider hiding cards instead of destroying and recreating the elements
+//   event.preventDefault()
+//   drawRecipeGrid(state.recipes.filter(r => r.name.toLowerCase().includes($searchvalue.value.toLowerCase())))
+// }
+
+// $searchform.onsubmit = onSearchSubmit
+
+// //Initial state
+// api.recipes.get()
+//   .then(data => state.recipes = data)
+//   .then(() => drawRecipeGrid(state.recipes))
+//   .then(_ => {
+//     // set search tags
+//     let container = D.getElementById("search-tags")
+//     alltags = new Set(state.recipes.map(r => r.tags.split(",")).flat().filter(e => e !== ""))
+
+//   api.ingredients.get().then(data => state.ingredients = data)
+//   api.recipe_ingredients.get().then(data => state.recipe_ingredients = data)
+//   api.mealplans.get().then(data => state.mealplans = data)
+//   })

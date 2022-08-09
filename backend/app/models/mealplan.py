53f00291e9665527bd1f4fa014f20baa7b326764
SimoHellgren <hellgrensimo@gmail.com>
2022-08-09 17:12:29 +0300

    add mealplan state

--- backend/app/models/mealplan.py
+++ backend/app/models/mealplan.py
@@ -1,6 +1,7 @@
-from sqlalchemy import Column, Date, ForeignKey, Integer, Numeric, String
+from sqlalchemy import Column, Date, ForeignKey, Integer, Numeric, String, Enum
 from sqlalchemy.orm import relationship
 from backend.app.db.base_class import Base
+from backend.app.enums import MealplanState
 from backend.app.models import Recipe
 
 
@@ -10,5 +11,6 @@ class Mealplan(Base):
     name = Column(String, nullable=False)
     servings = Column(Numeric)
     recipe_id = Column(Integer, ForeignKey("recipe.id"))
+    state = Column(Enum(MealplanState), default=MealplanState.open)
 
     recipe: Recipe = relationship(Recipe, back_populates="mealplans", uselist=False)

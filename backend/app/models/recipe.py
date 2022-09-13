5ffb02393789c73879abdd17c2223f5c1c0495cb
SimoHellgren <hellgrensimo@gmail.com>
2022-09-13 20:46:09 +0300

    add relationship from recipe to comment and modernize typehints

--- backend/app/models/recipe.py
+++ backend/app/models/recipe.py
@@ -1,10 +1,10 @@
 from sqlalchemy import Column, Integer, Numeric, String
 from sqlalchemy.orm import relationship
 from backend.app.db.base_class import Base
-from typing import TYPE_CHECKING, List
+from typing import TYPE_CHECKING
 
 if TYPE_CHECKING:
-    from backend.app.models import RecipeIngredient, Mealplan
+    from backend.app.models import RecipeIngredient, Mealplan, Comment
 
 
 class Recipe(Base):
@@ -15,9 +15,11 @@ class Recipe(Base):
     tags = Column(String)
     source = Column(String)
 
-    ingredients: List["RecipeIngredient"] = relationship(
+    ingredients: list["RecipeIngredient"] = relationship(
         "RecipeIngredient", back_populates="recipe", uselist=True
     )
-    mealplans: List["Mealplan"] = relationship(
+    mealplans: list["Mealplan"] = relationship(
         "Mealplan", back_populates="recipe", uselist=True
     )
+
+    comments: list["Comment"] = relationship("Comment")

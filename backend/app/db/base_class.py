a3e7525cdb8782dd8808b1389ab9b1b7478488ed
SimoHellgren <hellgrensimo@gmail.com>
2022-09-15 17:54:31 +0300

    timestamps to base model

--- backend/app/db/base_class.py
+++ backend/app/db/base_class.py
@@ -1,9 +1,14 @@
 from typing import Any, Dict
 from sqlalchemy.ext.declarative import as_declarative, declared_attr
+from sqlalchemy import Column, DateTime
+from sqlalchemy import func
 
 
 @as_declarative()
 class Base:
+    created = Column(DateTime, default=func.utc_timestamp())
+    updated = Column(DateTime, onupdate=func.utc_timestamp())
+
     # generate tablename automatically
     @declared_attr
     def __tablename__(cls) -> str:

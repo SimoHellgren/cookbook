50eb3d7fdea49d4b0e98aa7424980eaf0f74b731
SimoHellgren <hellgrensimo@gmail.com>
2022-10-05 14:50:12 +0300

    extract data loading

--- frontend/src/routes/recipes/+page.svelte
+++ frontend/src/routes/recipes/+page.svelte
@@ -1,20 +1,16 @@
 <script>
     import RecipeCard from './RecipeCard.svelte'
-    let recipes = fetch("http://127.0.0.1:8000/recipes/").then(response => recipes = response.json())
+    export let data;
 </script>
 
 
 <h1>Recipes</h1>
 
 <div class="recipecard-grid">
-    {#await recipes}
-        <p>Fetching data</p>
-    {:then recipes} 
-        {#each recipes as recipe}
-            <RecipeCard {...recipe}/>
-        {/each}
-    {/await}
-
+    {#each data.recipes as recipe}
+        <RecipeCard {...recipe}/>
+    {/each}
+    
 </div>
 
 <style>

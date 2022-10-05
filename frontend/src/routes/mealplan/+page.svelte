6b3103bdb6ebf104efff1319dd14e4520909f435
SimoHellgren <hellgrensimo@gmail.com>
2022-10-05 17:34:16 +0300

    mealplan stuff

--- frontend/src/routes/mealplan/+page.svelte
+++ frontend/src/routes/mealplan/+page.svelte
@@ -1,10 +1,11 @@
 <script>
+    import MealCard from "./MealCard.svelte";
     export let data;
+
 </script>
 
 <h1>Mealplans</h1>
-<ul>
+
     {#each data.mealplans as mealplan}
-        <li>{mealplan.date} {mealplan.name}</li>
+        <MealCard {...mealplan} recipe={data.recipes.find(r => r.id === mealplan.recipe_id) || {}}/>
     {/each}
-</ul>
---
+++ frontend/src/routes/mealplan/MealCard.svelte
@@ -0,0 +1,23 @@
+<script>
+    export let date;
+    export let name;
+    export let servings;
+    export let state;
+    export let recipe;
+</script>
+
+
+<div class="container">
+    {date} {name} ({servings}) {state} {recipe.name || ""}
+</div>
+
+
+<style>
+    .container {
+        background-color: #f4f4f4;
+        box-shadow: rgba(60, 64, 67, 0.3) 0px 1px 2px 0px, rgba(60, 64, 67, 0.15) 0px 1px 3px 1px;
+        margin: 1rem;
+        padding: 0.5rem;
+        width: fit-content;
+    }
+</style>

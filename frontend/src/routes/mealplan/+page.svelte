b71c86bb02e86285dff2d2380019f7165a0ae89e
SimoHellgren <hellgrensimo@gmail.com>
2022-10-05 21:20:51 +0300

    filter by date

--- frontend/src/routes/mealplan/+page.svelte
+++ frontend/src/routes/mealplan/+page.svelte
@@ -3,6 +3,9 @@
     import MealCard from "./MealCard.svelte";
     export let data;
 
+    let startdate;
+    let enddate;
+
     // group mealpans by date
     const groupByDate = (data) => {
         const m = new Map()
@@ -17,10 +20,23 @@
         return [...m].sort()
     }
 
-    $: grouped = groupByDate(data.mealplans)
+    $: grouped = groupByDate(
+        data.mealplans
+            .filter(mp => mp.date >= (startdate || "0000") && mp.date <= (enddate || "9999"))
+    )
 </script>
 
 <h1>Mealplans</h1>
+
+<h3>Filter</h3>
+<label>
+    Start
+    <input type="date" bind:value={startdate}>
+</label>
+<label>
+    End
+    <input type="date" bind:value={enddate}>
+</label>
 
 {#each grouped as [date, meals]}
     <MealCard {date} {meals}/>

329a9e10fb64091da39bde64e1d3a2bf63bf036f
SimoHellgren <hellgrensimo@gmail.com>
2022-10-06 15:58:01 +0300

    finalize recipe adding

--- frontend/src/routes/recipes/add/+page.svelte
+++ frontend/src/routes/recipes/add/+page.svelte
@@ -37,24 +37,39 @@
                     tags,
                     source,
                 }
-            ).then(r => r.json())
-        })
+            )
+        }).then(r => r.json())
         $recipes = [...$recipes, recipe]
 
         // create missing ingredients and update state
         const missing = recipe_ingredients.filter(ri => !$ingredients.find(i => i.name == ri.name))
         const new_ingredients = await Promise.all(
-            missing.map(ri => {
-                fetch("http://127.0.0.1:8000/ingredients", {
-                    method: "POST",
-                    headers: {'Content-Type': 'application/json'},
-                    body: JSON.stringify({name: ri.name})
+            missing.map(ri =>fetch("http://127.0.0.1:8000/ingredients", {
+                method: "POST",
+                headers: {'Content-Type': 'application/json'},
+                body: JSON.stringify({name: ri.name})
                 }).then(r => r.json())
-            })
+            )
         )
-        $ingredients = [...$ingredients, ...new_ingredients]
+        $ingredients = $ingredients.concat(new_ingredients)
 
         // connect ingredients with recipe
+        recipe_ingredients.forEach(ri => {
+            const data = {
+                recipe_id: recipe.id,
+                ingredient_id: $ingredients.find(i => i.name === ri.name).id,
+                quantity: ri.quantity,
+                measure: ri.measure,
+                optional: ri.optional,
+                position: ri.position,
+            }
+
+            fetch(`http://127.0.0.1:8000/recipes/${recipe.id}/ingredients`, {
+                method: "POST",
+                headers: {'Content-Type': 'application/json'},
+                body: JSON.stringify(data)
+            })
+        })
     }
 
 </script>

d343262460292a7da2e50e154776c75e03cb263f
SimoHellgren <hellgrensimo@gmail.com>
2022-10-06 14:51:46 +0300

    add ingredient store

--- frontend/src/stores.js
+++ frontend/src/stores.js
@@ -1,17 +1,37 @@
 import { writable } from 'svelte/store'
 
+const BASE = "http://127.0.0.1:8000"
+
 const initRecipes = () => {
   const { subscribe, set, update } = writable([]);
 
-  fetch("http://127.0.0.1:8000/recipes")
+  fetch(BASE + "/recipes")
     .then(response => response.json())
     .then(data => set(data))
 
   return {
     subscribe,
     set,
-    update
+    update,
   }
 }
 
 export const recipes = initRecipes();
+
+
+const initIngredients = () => {
+  const { subscribe, set, update } = writable([]);
+
+  fetch(BASE + "/ingredients")
+    .then(response => response.json())
+    .then(data => set(data))
+
+  return {
+    subscribe,
+    set,
+    update,
+  }
+}
+
+
+export const ingredients = initIngredients();

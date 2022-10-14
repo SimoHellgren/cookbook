44b455458a2c5793fd21c25dabdd83dc0bcc7993
SimoHellgren <hellgrensimo@gmail.com>
2022-10-14 21:03:42 +0300

    store utilizes api module

--- frontend/src/lib/stores/ingredients.js
+++ frontend/src/lib/stores/ingredients.js
@@ -1,26 +1,15 @@
+import api from '$lib/api'
 import { writable } from 'svelte/store';
-
-const BASE = 'http://127.0.0.1:8000/ingredients';
 
 const initIngredients = () => {
   const { subscribe, set, update } = writable([]);
 
-  fetch(BASE)
-    .then((response) => response.json())
-    .then((data) => set(data));
+  api.ingredients.getAll().then(data => set(data))
 
-  const add = async (data) => {
-    const response = await fetch(BASE, {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify(data),
-    });
-
-    const ingredient = await response.json();
-    update((state) => [...state, ingredient]);
-
-    return ingredient;
-  };
+  const add = async (data) => api.ingredients.create(data).then(d => {
+    update(state => [...state, d])
+    return d
+  })
 
   return {
     subscribe,
